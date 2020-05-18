f236ca02d7a132e072f2ae0ae0c2e0cb64b19265
Saulo Teodoro <saulotdr@gmail.com>
2020-05-18 08:19:31 -0300

    Modifying properties from JSON schema to proper fields

--- src/validation.py
+++ src/validation.py
@@ -1,59 +1,33 @@
-from fastjsonschema import compile as compile_schema
+from fastjsonschema import compile as compile_schema, validate as validate_schema
 
 """ @throws JsonSchemaException """
 schema = compile_schema({
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "title": "Scheduling Jobs Array",
-    "description": "Lista com pŕoximos jobs a serem executados",
     'type': 'array',
-    'required': ['ID', 'Descrição', 'Data Máxima de conclusão', 'Tempo estimado'],
-    'properties': {
-        'ID': {
-            'description': 'Identificador do job',
-            'type': 'number',
-            'minimum': 0
-        },
-        'Descrição': {
-            'description': 'Descrição do job',
-            'type': 'string'
-        },
-        'Data Máxima de conclusão': {
-            'description': 'Data máxima para execução do job',
-            'type': 'string',
-            'format': 'date'
-        },
-        'Tempo estimado': {
-            'description': 'Duração estimada do job',
-            'type': 'string',
-            'pattern': '\\d.+hora|horas'
+    "items": {
+        "type": "object",
+        'required': ['ID', 'Descrição', 'Data Máxima de conclusão', 'Tempo estimado'],
+        'properties': {
+            'ID': {
+                'description': 'Identificador do job',
+                'type': 'number',
+                'minimum': 0
+            },
+            'Descrição': {
+                'description': 'Descrição do job',
+                'type': 'string'
+            },
+            'Data Máxima de conclusão': {
+                'description': 'Data máxima para execução do job',
+                'type': 'string'
+            },
+            'Tempo estimado': {
+                'description': 'Duração estimada do job',
+                'type': 'string',
+                'pattern': '\\d.+hora|horas'
+            }
         }
     }
 })
 
-# """ @throws JsonSchemaException """
-# window = compile_schema({
-#     "$schema": "http://json-schema.org/draft-07/schema#",
-#     "title": "Scheduling Jobs Array",
-#     "description": "Lista com pŕoximos jobs a serem executados",
-#     'type': 'array',
-#     'required': ['ID', 'Descrição', 'Data Máxima de conclusão', 'Tempo estimado'],
-#     'properties': {
-#         'ID': {
-#             'description': 'Identificador do job',
-#             'type': 'number',
-#             'minimum': 0
-#         },
-#         'Descrição': {
-#             'description': 'Descrição do job',
-#             'type': 'string'
-#         },
-#         'Data Máxima de conclusão': {
-#             'description': 'Data máxima para execução do job',
-#             'type': 'string'
-#         },
-#         'Tempo estimado': {
-#             'description': 'Duração estimada do job',
-#             'type': 'string'
-#         }
-#     }
-# })
+def validate(payload):
+    schema(payload)
